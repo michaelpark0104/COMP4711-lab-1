b3ecc7ff8626a0412ffe6636f719d997beb0f0d4
Michael Park <michaelpark0104@gmail.com>
2017-01-12 17:59:57 -0800

    Added Student class

--- Student.php
+++ Student.php
@@ -13,6 +13,7 @@
  */
 class Student {
     
+    //constructer
     function __construct() {
         $this->surname = '';
         $this->first_name = '';
@@ -20,14 +21,17 @@ class Student {
         $this->grades = array();
     }
     
+    //add email
     function add_email($which,$address) {
         $this->emails[$which] = $address;
     }
 
+    //add grade
     function add_grade($grade) {
         $this->grades[] = $grade;
     }
     
+    //takes average of a student's grade
     function average() {
         $total = 0;
         foreach ($this->grades as $value)
@@ -35,6 +39,7 @@ class Student {
         return $total / count($this->grades);
     }
     
+    //displaying student records
     function toString() {
         $result = $this->first_name . ' ' . $this->surname;
         $result .= ' ('.$this->average().") \n";
