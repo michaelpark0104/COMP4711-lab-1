0f175c68459b26cb975dadb13e2416730d8e30ff
Michael Park <michaelpark0104@gmail.com>
2017-01-12 17:39:07 -0800

    Added Student class

--- Student.php
+++ Student.php
@@ -37,7 +37,7 @@ class Student {
     
     function toString() {
         $result = $this->first_name . ' ' . $this->surname;
-        $result .= ' ('.$this->average().")\n";
+        $result .= ' ('.$this->average().") \n";
         foreach($this->emails as $which=>$what)
             $result .= $which . ': '. $what. "\n";
         $result .= "\n";
